cf47e666acfd3d23ff81bf42839d54aad1c5a22c
Benjamin Schmitz <vortex@wolpzone.de>
2012-10-10 11:29:14 +0200

    add intellij project for music server

--- MusicServer/src/de/pandaserv/music/server/Main.java
+++ MusicServer/src/de/pandaserv/music/server/Main.java
@@ -1,5 +1,7 @@
 package de.pandaserv.music.server;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Properties;
 import org.slf4j.Logger;
@@ -14,7 +16,15 @@ public class Main {
 
         // load startup configuration
         Properties startupConfig = new Properties();
-        InputStream configIn = Thread.currentThread().getContextClassLoader().getResourceAsStream("startup.cfg");
+        InputStream configIn;
+        // try to find config in working directory
+        File configFile = new File("startup.cfg");
+        if (configFile.exists()) {
+            configIn = new FileInputStream(configFile);
+        } else {
+            // try to find config on classpath
+            configIn = Thread.currentThread().getContextClassLoader().getResourceAsStream("startup.cfg");
+        }
         if (configIn == null) {
             logger.error("FATAL: unable to locate startup configuration file 'startup.cfg'");
             System.exit(1);
