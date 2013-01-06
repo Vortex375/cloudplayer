0005fa2bbda89e8d04f2b9299e60a07f59a2e7a2
Benjamin Schmitz <vortex@wolpzone.de>
2013-01-06 18:13:14 +0100

    range queries for stream service

--- MusicServer/src/de/pandaserv/music/server/service/StreamService.java
+++ MusicServer/src/de/pandaserv/music/server/service/StreamService.java
@@ -19,7 +19,7 @@ import java.util.StringTokenizer;
 
 public class StreamService extends AbstractHandler {
 
-    private static final int STREAM_BUFFER_SIZE = 256;
+    private static final int STREAM_BUFFER_SIZE = 5120; // 5kB
 
     static final Logger logger = LoggerFactory.getLogger(StreamService.class);
 
@@ -59,21 +59,70 @@ public class StreamService extends AbstractHandler {
             return;
         }
 
+        long length = CacheManager.getInstance().getSize(id);
+
+        // parse requested range
+        long rangeStart;
+        long rangeEnd;
+        String rangeHeader = request.getHeader("Range");
+        if (rangeHeader == null) {
+            rangeStart = 0;
+            rangeEnd = length - 1;
+        } else if (!rangeHeader.startsWith("bytes=")) {
+            fail(HttpServletResponse.SC_BAD_REQUEST, "The only accepted Range is \"byte\"",
+                    baseRequest, response);
+            return;
+        } else {
+            String[] rangeStrings = rangeHeader.substring(6).split("-");
+            try {
+                rangeStart = Long.parseLong(rangeStrings[0]);
+                if (rangeStrings.length < 2 || rangeStrings[1].equals("*")) {
+                    rangeEnd = length - 1;
+                } else {
+                    rangeEnd = Long.parseLong(rangeStrings[1]);
+                }
+            } catch (NumberFormatException e) {
+                fail(HttpServletResponse.SC_BAD_REQUEST, "Unable to parse Range header.",
+                        baseRequest, response);
+                return;
+            }
+        }
+
+        // check for valid ranges
+        if (rangeEnd < rangeStart) {
+            fail(HttpServletResponse.SC_BAD_REQUEST, "Invalid ranges specified in request.",
+                    baseRequest, response);
+            return;
+        }
+        if (rangeEnd > length) {
+            rangeEnd = length - 1;
+        }
+
         InputStream inStream = CacheManager.getInstance().getInputStream(id);
         OutputStream outStream = response.getOutputStream();
 
-        // begin streaming
+
+        // setHeaders
         response.setStatus(HttpServletResponse.SC_OK);
+        response.setHeader("Accept-Ranges", "bytes");
         response.setHeader("Content-Type", "audio/mpeg"); //TODO: detect/retrieve format!!
-        response.setHeader("Content-Length", "" + CacheManager.getInstance().getSize(id));
+        response.setHeader("Content-Length", "" + (rangeEnd - rangeStart + 1));
+        response.setHeader("Content-Range", "bytes " + rangeStart + "-" + rangeEnd + "/" + length);
         baseRequest.setHandled(true);
-        //TODO: how to do this properly?
+
+        // begin streaming task
         byte[] buf = new byte[STREAM_BUFFER_SIZE];
+        long streamLength = rangeEnd - rangeStart + 1;
         int read;
         logger.info("streaming task started for stream " + id);
+        long skip = inStream.skip(rangeStart);
+        logger.info("Skipped " + skip + " bytes from InputStream.");
         try {
             do {
-                read = inStream.read(buf);
+                read = inStream.read(buf, 0, (int) Math.min(buf.length, streamLength));
+                streamLength -= read;
+                //rangeStart += read; // for debug output
+                logger.info("sending " +  read + " bytes (" + streamLength + " remaining)");
                 outStream.write(buf, 0, read);
             } while (read == buf.length);
             outStream.flush();
