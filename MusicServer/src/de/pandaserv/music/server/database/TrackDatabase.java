908f5404571a58eb57e4c7f765a8a956ef78f456
Benjamin Schmitz <vortex@wolpzone.de>
2013-02-26 02:35:29 +0100

    use filename instead of title when not available

--- MusicServer/src/de/pandaserv/music/server/database/TrackDatabase.java
+++ MusicServer/src/de/pandaserv/music/server/database/TrackDatabase.java
@@ -71,10 +71,23 @@ public class TrackDatabase {
                 + " FROM Tracks"
                 + " WHERE id=?");
         trackQuerySimple = new LocalPreparedStatement("" +
-                "SELECT id, title, artist, album" +
+                "SELECT id, title, artist, album, device_path" +
                 " FROM Tracks" +
                 " WHERE LOWER(title) LIKE LOWER(?) OR LOWER(artist) LIKE LOWER(?) or LOWER(album) LIKE LOWER(?) or LOWER(device_path) LIKE LOWER(?)" +
                 " ORDER BY device_path");
+        //TODO: this is ugly, especially querying twice - need to optimize
+        /*trackQuerySimple = new LocalPreparedStatement("" +
+                "(SELECT id, title, artist, album" +
+                " FROM Tracks" +
+                " WHERE (LOWER(title) LIKE LOWER(?) OR LOWER(artist) LIKE LOWER(?) or LOWER(album) LIKE LOWER(?) or LOWER(device_path) LIKE LOWER(?))" +
+                "    AND NOT title = ''" +
+                " ORDER BY device_path)" +
+                "UNION" +
+                "(SELECT id, RIGHT(device_path, POSITION('/' IN REVERSE(device_path)) - 1) as title, artist, album" +
+                " FROM Tracks" +
+                " WHERE (LOWER(title) LIKE LOWER(?) OR LOWER(artist) LIKE LOWER(?) or LOWER(album) LIKE LOWER(?) or LOWER(device_path) LIKE LOWER(?))" +
+                "    AND title = ''" +
+                " ORDER BY device_path)");*/
     }
 
     public String[] getDeviceAndPath(long id) {
@@ -143,6 +156,11 @@ public class TrackDatabase {
             while(rs.next()) {
                 long id = rs.getLong(1);
                 String title = rs.getString(2);
+                // replace title with filename when not available
+                if (title.equals("")) {
+                    title = rs.getString(5);
+                    title = title.substring(title.lastIndexOf("/") + 1, title.length());
+                }
                 String artist = rs.getString(3);
                 String album = rs.getString(4);
                 ret.add(new TrackDetail(id, title, artist, album));
@@ -169,13 +187,19 @@ public class TrackDatabase {
                 Track track = new Track();
                 track.setId(rs.getLong(1));
                 track.setDevice(rs.getString(2));
-                track.setTitle(rs.getString(3));
+                String title = rs.getString(3);
+                // replace title with filename when not available
+                if (title.equals("")) {
+                    title = rs.getString(9);
+                    title = title.substring(title.lastIndexOf("/") + 1, title.length());
+                }
+                track.setTitle(title);
                 track.setArtist(rs.getString(4));
                 track.setAlbum(rs.getString(5));
                 track.setGenre(rs.getString(6));
                 track.setTrack(rs.getInt(7));
                 track.setYear(rs.getInt(8));
-                track.setDevice(rs.getString(9));
+                track.setDevicePath(rs.getString(9));
                 return track;
             } else {
                 return null;
