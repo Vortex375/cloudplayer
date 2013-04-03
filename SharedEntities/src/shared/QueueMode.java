b7a40a56c1025ec0dfce22ee706d447f4ef0b6d9
Benjamin Schmitz <benjamin.schmitz@uni-ulm.de>
2013-04-03 14:52:14 +0200

    missing class

---
+++ SharedEntities/src/shared/QueueMode.java
@@ -0,0 +1,71 @@
+package de.pandaserv.music.shared;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ich
+ * Date: 3/28/13
+ * Time: 11:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class QueueMode {
+    public static class Other {
+
+    }
+
+    public static class Playlist {
+        private String playlist;
+
+        public Playlist() {
+        }
+
+        public Playlist(String playlist) {
+            this.playlist = playlist;
+        }
+
+        public String getPlaylist() {
+            return playlist;
+        }
+
+        public void setPlaylist(String playlist) {
+            this.playlist = playlist;
+        }
+    }
+
+    public static class AlbumOf {
+        private long id;
+
+        public AlbumOf() {
+        }
+
+        public AlbumOf(long id) {
+            this.id = id;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public void setId(long id) {
+            this.id = id;
+        }
+    }
+
+    public static class Query {
+        private String query; //TODO: string?
+
+        public Query() {
+        }
+
+        public Query(String query) {
+            this.query = query;
+        }
+
+        public String getQuery() {
+            return query;
+        }
+
+        public void setQuery(String query) {
+            this.query = query;
+        }
+    }
+}
