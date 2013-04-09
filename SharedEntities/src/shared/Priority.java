450d26d90c5d7bed6010133a24158dcb79a25d63
Benjamin Schmitz <vortex@wolpzone.de>
2013-04-09 02:34:43 +0200

    missing class

---
+++ SharedEntities/src/shared/Priority.java
@@ -0,0 +1,14 @@
+package de.pandaserv.music.shared;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ich
+ * Date: 4/9/13
+ * Time: 12:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Priority {
+        HIGH,
+        NORMAL,
+        LOW
+}
